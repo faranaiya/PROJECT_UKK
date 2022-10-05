09a3e3da165fa5b71f06118dc9679f9c821300be
Fara1sampai9 <95529650+Fara1sampai9@users.noreply.github.com>
2022-10-05 11:28:55 +0700

    aplikasispp

---
+++ datasiswa.php
@@ -0,0 +1,45 @@
+<?php 
+include 'koneksi.php';
+
+include 'header4.php';
+ ?>
+<div class="container">
+	<div class="page-header">
+<h2> DATA SISWA SMK PLUS PELITA NUSANTARA</h2>
+	</div>
+<a class="btn btn-primary " href="tambahSW.php">TAMBAH DATA</a>
+ <br/> <br>
+ <table class="table table-bordered table-striped">
+ 	<tr>
+ 		<th>NO</th>
+ 		<th>ID SISWA</th>
+ 		<th>NIS</th>
+ 		<th>NAMA SISWA</th>
+ 		<th>KELAS</th>
+		<th>TAHUN AJARAN</th>
+		<th>BIAYA</th>
+ 	</tr>
+ 	<?php 
+ 	$data = $konek ->query("SELECT * FROM `siswa` WHERE 1");
+ 	$i=1;
+ 	while ($dta = mysqli_fetch_assoc($data) ) : 
+ 	 ?>
+ 	 <tr>
+ 	 	<td><?= $i; ?></td>
+ 	 	<td><?= $dta['idsiswa'] ?></td>
+ 	 	<td><?= $dta['nis'] ?></td>
+ 	 	<td><?= $dta['namasiswa'] ?></td>
+ 	 	<td><?= $dta['kelas'] ?></td>
+ 	 	<td><?= $dta['tahunajaran'] ?></td>
+		<td><?= $dta['biaya'] ?></td>
+ 	 	<td>
+ 	 		<a class="btn btn-warning btn-sm" href="ubahSW.php?= $dta['id'] ?>">EDIT</a> 
+ 	 		<a class="btn btn-danger btn-sm" href="hapusSW.php?= $dta['id'] ?>" onclick ="return confirm('apakah anda yakin ingin menghapus data? data SPP Siswa yang bersangkutan akan ikut terhapus')">HAPUS</a>
+ 	 	</td>
+ 	 </tr>
+ 	 <?php $i++;  ?>
+ 	<?php endwhile; ?>
+ </table>
+ <p align="center" style="font-family: arial; color: red; size: 10px;">Menghapus Data Siswa Maka Akan menghapus Data Pembayaran dan data tagihan Siswa pada tabel SPP</p>
+ </div>
+ <?php include 'footer.php'; ?>
---
+++ index1.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Aplikasi Pembayaran SPP</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <!-- Bootstrap Core CSS -->
+	<link href="http://localhost/spps/media//css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="http://localhost/spps/media//css/load-font-googleapis.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="http://localhost/spps/media//css/font-awesome.min.css">
+
+
+	<!-- Custom CSS -->
+	<link href="http://localhost/spps/media//css/frontend-style.css" rel="stylesheet">
+	<link href="http://localhost/spps/media//css/portal.css" rel="stylesheet">
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Background Video-->
+        <video class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="assets/mp4/bg.mp4" type="video/mp4" /></video>
+        <!-- Masthead-->
+        <div class="masthead">
+            <div class="masthead-content text-white">
+                <div class="container-fluid px-4 px-lg-0">
+                    <h2><i class="fa fa-graduation-cap"></i> Aplikasi pembayaran SPP</h2>
+                    <p class="mb-5">school education payment system application at Smk Plus Pelita Nusantara</p>
+                   
+                    <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                        <body>
+
+                            <!-- Home -->
+                            <section class="content-section">
+                                <div class="container text-center">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            
+                                            <p class="lead mb-5 colr">Sistem Pembayaran Pendidikan Sekolah</p>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="index.php">
+                                            <div class="box">
+                                                <i class="fa fa-desktop icon-menu"></i>
+                                                <br>
+                                                Login Admin
+                                            </div>
+                                        </a>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="http://localhost/spps/home">
+                                            <div class="box">
+                                                <i class="fa fa-credit-card icon-menu"></i>
+                                                <br>
+                                                Login Petugas
+                                            </div>
+                                        </a>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="siswa/index.php">
+                                            <div class="box">
+                                                <i class="fa fa-users icon-menu"></i>
+                                                <br>
+                                                Login Siswa
+                                            </div>
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
+                        
+                        
+                        </body>
+                        
+                        </html>
+                      
+        <!-- Social Icons-->
+        <!-- For more icon options, visit https://fontawesome.com/icons?d=gallery&p=2&s=brands-->
+        <div class="social-icons">
+            <div class="d-flex flex-row flex-lg-column justify-content-center align-items-center h-100 mt-3 mt-lg-0">
+                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-twitter"></i></a>
+                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-facebook-f"></i></a>
+                <a class="btn btn-dark m-3" href="#!"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
